386d3b40d2365fef1d734ff41fd65ef2a03e5054
katarinasupe <supe.katarina@gmail.com>
2021-12-14 13:14:58 +0100

    Create kafka stream

--- backend/server/setup.py
+++ backend/server/setup.py
@@ -7,9 +7,9 @@ log = logging.getLogger(__name__)
 
 def connect_to_memgraph(memgraph_ip, memgraph_port):
     memgraph = Memgraph(host=memgraph_ip, port=int(memgraph_port))
-    while(True):
+    while True:
         try:
-            if (memgraph._get_cached_connection().is_active()):
+            if memgraph._get_cached_connection().is_active():
                 return memgraph
         except:
             log.info("Memgraph probably isn't running.")
@@ -26,16 +26,19 @@ def run(memgraph):
             """CREATE TRIGGER pagerank_trigger 
                BEFORE COMMIT 
                EXECUTE CALL pagerank_online.update(createdVertices, createdEdges, deletedVertices, deletedEdges) YIELD *
-               SET node.rank = rank;""")
+               SET node.rank = rank;"""
+        )
 
         log.info("Creating stream connections on Memgraph")
         memgraph.execute(
-            "CREATE STREAM retweets TOPICS retweets TRANSFORM twitter.tweet")
+            "CREATE KAFKA STREAM retweets TOPICS retweets TRANSFORM twitter.tweet"
+        )
         memgraph.execute("START STREAM retweets")
 
         log.info("Creating triggers on Memgraph")
         memgraph.execute(
-            "CREATE TRIGGER created_trigger ON CREATE AFTER COMMIT EXECUTE CALL publisher.create(createdObjects)")
+            "CREATE TRIGGER created_trigger ON CREATE AFTER COMMIT EXECUTE CALL publisher.create(createdObjects)"
+        )
     except Exception as e:
         log.info(f"Error on stream and trigger creation: {e}")
         pass
