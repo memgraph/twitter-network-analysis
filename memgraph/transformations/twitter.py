e9fe677e7232ee21819f1c856d84aea84f83f2d9
katarinasupe <supe.katarina@gmail.com>
2022-02-23 15:36:57 +0100

    Add directed relationship to MERGE

--- memgraph/transformations/twitter.py
+++ memgraph/transformations/twitter.py
@@ -3,26 +3,33 @@ import json
 
 
 @mgp.transformation
-def tweet(messages: mgp.Messages
-          ) -> mgp.Record(query=str, parameters=mgp.Nullable[mgp.Map]):
+def tweet(
+    messages: mgp.Messages,
+) -> mgp.Record(query=str, parameters=mgp.Nullable[mgp.Map]):
     result_queries = []
 
     for i in range(messages.total_messages()):
         message = messages.message_at(i)
-        tweet_dict = json.loads(message.payload().decode('utf8'))
+        tweet_dict = json.loads(message.payload().decode("utf8"))
         if tweet_dict["target_username"]:
             result_queries.append(
                 mgp.Record(
-                    query=("MERGE (u1:User {username: $source_username}) "
-                           "MERGE (u2:User {username: $target_username}) "
-                           "MERGE (u1)-[:RETWEETED]-(u2)"),
+                    query=(
+                        "MERGE (u1:User {username: $source_username}) "
+                        "MERGE (u2:User {username: $target_username}) "
+                        "MERGE (u1)-[:RETWEETED]->(u2)"
+                    ),
                     parameters={
                         "source_username": tweet_dict["source_username"],
-                        "target_username": tweet_dict["target_username"]}))
+                        "target_username": tweet_dict["target_username"],
+                    },
+                )
+            )
         else:
             result_queries.append(
                 mgp.Record(
                     query=("MERGE (:User {username: $source_username})"),
-                    parameters={
-                        "source_username": tweet_dict["source_username"]}))
+                    parameters={"source_username": tweet_dict["source_username"]},
+                )
+            )
     return result_queries
